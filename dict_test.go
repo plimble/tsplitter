30ef061c44c103cb17783e2201dd8757126e8169
Witoo Harianto <witoo@outlook.com>
2015-02-09 23:15:35 +0700

    add test

--- dict_test.go
+++ dict_test.go
@@ -24,6 +24,7 @@ func TestFileDictExist(t *testing.T) {
 	dict := NewFileDict("dictionary.txt")
 	assert.True(t, dict.Exist("นั้น"))
 	assert.False(t, dict.Exist("นั้น1"))
+	assert.False(t, dict.Exist(""))
 }
 
 func BenchmarkFileDictExist(b *testing.B) {
